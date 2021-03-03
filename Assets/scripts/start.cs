82644c0d26b1649f9bbfc3c614aee63446fbb457
satyamsingh7058 <74784722+satyamsingh7058@users.noreply.github.com>
2021-03-03 13:10:19 +0530

    hii

---
+++ Assets/scripts/start.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start : MonoBehaviour
+{
+    public void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("GDD GAME");
+    }
+}
